f9edd0dd33e1d5175ccf60af60aafabb9e1f2a42
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-29 22:40:06 +0000

    Add spec and src

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,9 +1,19 @@
-describe("Fizzbuzz", function() {
-  var fizzbuzz;
-  describe("Know when a number is", function(){
-    it("divisible by 3", function(){
-      fizzbuzz = new Fizzbuzz()
-      expect(isDivisibleByThree(3)).toBe(ture)
-    });
+describe("fizzBuzz", function() {
+
+  it("should return 'fizz' when given a number divisible by 3", function() {
+    expect(fizzBuzz(3)).toEqual("fizz");
   });
+
+  it("should return 'buzz' when given a number divisible by 5", function() {
+    expect(fizzBuzz(5)).toEqual("buzz");
+  });
+
+  it("should return 'fizzBuzz' when given a number divisible by 15", function() {
+    expect(fizzBuzz(15)).toEqual("fizzBuzz");
+  });
+
+  it("should return the given number when it is not divisible by 3 or 5", function() {
+    expect(fizzBuzz(1)).toEqual(1);
+  });
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,4 +1,11 @@
-
-var Fizzbuzz = function(){
-
-};
+function fizzBuzz(num) {
+  if (num % 15 === 0) {
+    return 'fizzBuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else {
+    return num;
+  }
+}
